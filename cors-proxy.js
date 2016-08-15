cad5d13bd1b3843f5a3a83c0d268277ef252bb1f
Adrien Jarthon <jobs@adrienjarthon.com>
2016-08-15 23:09:36 +0200

    Initial commit

---
+++ cors-proxy.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+var express = require('express');
+var request = require('request');
+
+var app = express();
+app.use('/', function(req, res) {
+  var url = apiServerHost + req.url;
+  req.pipe(request(url)).pipe(res);
+});
+
+app.listen(process.env.PORT || 3000);
