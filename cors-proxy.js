999b33c1da45e6aaaf586f7345715afec55902f7
Adrien Jarthon <jobs@adrienjarthon.com>
2016-08-16 09:32:32 +0200

    working cors proxy + profile fetching

--- cors-proxy.js
+++ cors-proxy.js
@@ -3,10 +3,22 @@
 var express = require('express');
 var request = require('request');
 
+var apiServerHost = "https://api.t411.ch";
 var app = express();
 app.use('/', function(req, res) {
   var url = apiServerHost + req.url;
-  req.pipe(request(url)).pipe(res);
+  console.log(`→ ${url} (${req.headers["authorization"]})`);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Authorization");
+  res.header("Cache-Control", "max-age=600");
+  options = {
+    url: url,
+    headers: {
+      "Authorization": req.headers["authorization"],
+      "User-Agent": "curl/7.47.0"
+    }
+  }
+  req.pipe(request(options)).pipe(res);
 });
 
 app.listen(process.env.PORT || 3000);
