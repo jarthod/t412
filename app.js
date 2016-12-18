2907ae216bee35a0f173ab78415e46a42f7a2729
Adrien Jarthon <jobs@adrienjarthon.com>
2016-12-18 12:11:05 +0100

    update t411 domain-name

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 var express = require('express');
 var request = require('request');
 
-var apiServerHost = "https://api.t411.ch";
+var apiServerHost = "https://api.t411.li";
 var app = express();
 
 app.get('/', (req,res) => { res.sendFile('t412.html', {root: __dirname}); });
